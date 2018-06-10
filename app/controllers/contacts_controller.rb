d6e3c634e3607ea2b1a1695be14c1ecc4d6ec2d9
segudrid <segudrid@gmail.com>
2018-06-10 02:30:31 -0400

    accion de crear contact para guardarlo en base de datos añadida al controlador

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,20 @@
 class ContactsController < ApplicationController
-
-    def new
-    end 
+   def new
+     @contact = Contact.new
+   end 
   
+   def create
+     @contact = Contact.new(contact_params)
+     if @contact.save
+       redirect_to new_contact_path, notice: "Mensaje recibido"
+     else
+       redirect_to new_contact_path, notice: "Error al envíar mensaje"
+     end
+   end
+  
+  private
+    def contact_params
+     params.require(:contact).permit(:nombre, :correo, :comentarios)
+   end
 end
 
